8757a2c9a3d1185407354862d48b6901dd1d49c1
mukunda <suryaysr69@gmail.com>
2023-02-27 04:29:46 +0530

    s3 state

---
+++ s3-state/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform71"
+    key= "s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0730089cc7ff79932"]
+  tags = {
+    name="test"
+  }
+}
