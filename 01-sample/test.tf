e480058776e738efdefc723b8b4b48b55ec984de
mukunda <suryaysr69@gmail.com>
2023-02-26 19:06:23 +0530

    learn-terraform

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0730089cc7ff79932]
+  vpc_security_group_ids = ["sg-0730089cc7ff79932"]
   tags = {
     name="test"
   }
