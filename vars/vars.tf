3be7a881f9a316c87185a1b68ec04b5d4c4ab00c
mukunda <suryaysr69@gmail.com>
2023-03-10 15:49:24 +0530

    vars.tf

---
+++ vars/vars.tf
@@ -0,0 +1,14 @@
+variable "d1" {
+  default = [
+    {
+      course_name  = "aws"
+      trainer_name = "raju"
+    },
+    {
+      course_name = "devops"
+      trainer_name = "ravi"
+      course_name  = "devops"
+      trainer_name = "ravi"
+    }
+  ]
+}
